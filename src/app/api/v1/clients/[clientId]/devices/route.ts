d2b49f24d297c8078b9bfe98ad94cb9d6deab60c
Francisco Burgoa <franciscojburgoa@gmail.com>
2025-09-09 13:14:57 -0300

    refactor: simplify parameter structure in GET function for improved clarity

--- src/app/api/v1/clients/[clientId]/devices/route.ts
+++ src/app/api/v1/clients/[clientId]/devices/route.ts
@@ -5,10 +5,7 @@ import { fetchDevicesWithLatestReadingsForClient } from "@/lib/data";
 
 export const dynamic = "force-dynamic";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { clientId: string } }
-) {
+export async function GET(request: NextRequest, params: { clientId: string }) {
   const supabase = await createClient();
   const clientIdFromUrl = params.clientId;
 
