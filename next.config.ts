e90b6f08b337559166c2e955af91c52d455faef2
Francisco Burgoa <franciscojburgoa@gmail.com>
2025-09-09 13:27:02 -0300

    refactor: enhance webpack configuration to ignore Supabase Edge Functions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  webpack: (config) => {
+    // Excluir archivos de Supabase Edge Functions
+    config.module.rules.push({
+      test: /supabase\/functions/,
+      loader: "ignore-loader",
+    });
+    return config;
+  },
+
+  // O usar exclude en TypeScript
+  exclude: ["**/supabase/functions/**/*"],
 };
 
 export default nextConfig;
